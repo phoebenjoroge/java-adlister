1d9d969181e3de124397d68d31eaee70bc1707f7
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-03-08 14:01:14 -0600

    updating hello jsp exercise

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class HelloServlet {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String hello = request.getParameter("said");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        if (hello != null) {
+            out.printf("<h1>User said: %s</h1>", hello);
+        } else {
+            out.println("<h1>Hello World!! </h1>");
+        }
+    }
 }
