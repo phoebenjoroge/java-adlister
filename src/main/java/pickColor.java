2b13c60269554968ac9488d1e917db4d27c773e8
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-03-10 10:01:16 -0600

    updating jsp exercise

--- src/main/java/pickColor.java
+++ src/main/java/pickColor.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class pickColor {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "pickColor", urlPatterns = "/pickcolor")
+public class pickColor extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        request.getRequestDispatcher("/pickcolor.jsp").forward(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String favoriteColor = request.getParameter("favorite_color");
+
+        response.sendRedirect("/viewcolor?favorite_color=" + favoriteColor);
+    }
 }
+
